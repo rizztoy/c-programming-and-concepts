82ab9c7004081075f29445d8ea03a8ee248778b9
Soni Ritika <136151419+rizztoy@users.noreply.github.com>
2023-10-05 00:31:35 -1000

    hello world.c

--- hello world.c
+++ hello world.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+// double slash is the way you can comment a sentense.
+// stdio. h is a header file which has the necessary information to include the input/output related functions in our program. Example printf, scanf
 main()
+// A main is a predefined keyword or function in C.
+// It is the first function of every C program that is responsible for starting the execution and termination of the program.
 {
   printf("Hello World!!");
+  // It prints the output in console when the program is compiled and then when we run it..
+  return 0;
 }
